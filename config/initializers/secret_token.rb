76e830461a72fb279626ae57d8a696e00374841d
Joram Okwaro <joram@kopokopo.com>
2011-12-06 11:49:00 +0300

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Famag::Application.config.secret_token = '4cecc056828f3518a3dd459cbc4e77d8a5c7c224442c62911de1974c6f4feb0e927409b4e198e3e412d98bc3743933e636242a1541c480967b80a1548433fdd5'
